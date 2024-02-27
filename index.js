f1368b16b6e29f09f4d3abc4764d04d6b2e84c34
srikanthmallam <sriaknthmallam0@gmail.com>
2024-02-28 01:04:03 +0530

    updated client link

--- index.js
+++ index.js
@@ -13,7 +13,12 @@ const {notFound,errorHandler} = require("./middleware/errorMiddleware")
 const app = express();
 app.use(express.json({extended:true}))
 app.use(express.urlencoded({extended:true}))
-app.use(cors({credentials:true,origin:"http://localhost:3000"}))
+app.use(
+  cors({
+    credentials: true,
+    origin: "https://blog-app-client-22gv.onrender.com",
+  })
+);
 app.use(upload())
 app.use('/uploads',express.static(__dirname + '/uploads'))
 
