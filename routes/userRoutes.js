4c6f35489db0779699717176f88ca3dc8fe896f0
srikanthmallam <sriaknthmallam0@gmail.com>
2024-03-03 13:13:11 +0530

    removed env file

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,15 +9,15 @@ const {
   getUser,
 } = require("../controller/usercontroller");
 
-const authMiddelware = require('../middleware/auth')
+const authMiddelware = require("../middleware/auth");
 
 const router = Router();
 
-router.post("/register" ,registerUser)
-router.post("/login" ,loginUser)
-router.get("/:id",getUser)
-router.get("/",getAuthors)
-router.post('/change-avatar',authMiddelware,chanageAvatar)
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.get("/:id", getUser);
+router.get("/", getAuthors);
+router.post("/change-avatar", authMiddelware, chanageAvatar);
 router.patch('/edit-user',authMiddelware,editUserDetails)
 
 module.exports = router;
